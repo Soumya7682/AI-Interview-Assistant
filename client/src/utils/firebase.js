9a37b08292edfeb4cbfbff3d605a8ec9ffa8d2c8
soumya <singhsoumyaa31@gmail.com>
2026-06-20 22:44:29 +0530

    Updated pricing page and firebase config

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "fir-52e3c.firebaseapp.com",
-  projectId: "fir-52e3c",
-  storageBucket: "fir-52e3c.firebasestorage.app",
-  messagingSenderId: "541598709233",
-  appId: "1:541598709233:web:0ba841a6296a0c42400050"
+  authDomain: "interviewassist-270ad.firebaseapp.com",
+  projectId: "interviewassist-270ad",
+  storageBucket: "interviewassist-270ad.firebasestorage.app",
+  messagingSenderId: "320861523982",
+  appId: "1:320861523982:web:f775709820f56b89feec5f"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,3 +17,5 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider()
 
 export {auth , provider}
+
+// i already push my project into github anf deploy in the render before 1month but now i change my .env file show how can update it on github and render
